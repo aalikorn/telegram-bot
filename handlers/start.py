6b157d5809c6a32f1f2137fd3d58233c22cff314
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2024-06-20 09:24:51 +0300

    изменен текст старт

--- handlers/start.py
+++ handlers/start.py
@@ -17,15 +17,17 @@ async def start(message: Message, state: FSMContext):
     await message.answer(text=text, reply_markup=form_button)
     # await bot.send_photo(chat_id=message.chat.id, photo="images/2024-06-15 09.21.52.jpg", caption="Фото")
     # images = ['images/2024-06-15 09.21.52.jpg', 'images/2024-06-15 10.17.37.jpg']
-    photo = await get_photo("имя фотографии")
-    await message.answer_photo(photo=photo)
+    # photo = await get_photo("имя фотографии")
+    # await message.answer_photo(photo=photo)
     # await message.answer(text="Привет! Нажми на кнопку ниже, чтобы открыть меню", reply_markup=menu_button)
     # question = 'Какой-то вопрос'
     # options = ['вариант 1', 'вариант 2']
     # await message.answer_poll(question=question, options=options, is_anonymous=False)
     # await message.delete()
-    images = [InputMediaPhoto(media=await get_photo("")), ]
-    await message.answer_media_group(media=images)
+    # images = [InputMediaPhoto(media=await get_photo("")), ]
+    # await message.answer_media_group(media=images)
+    dice = await message.answer_dice()
+    print(dice.dice.value)
     # # await message.answer_location(latitude=55.75350826419063, longitude=48.74339838777191)
     # text = "другой текст `этот текст копируется при нажатии`"
     # await message.answer(text=text, parse_mode='MarkdownV2')
@@ -33,7 +35,7 @@ async def start(message: Message, state: FSMContext):
 @router.poll_answer()
 async def poll_answer(poll_answer: PollAnswer):
     answer_ids = poll_answer.user
-    print(answer_ids)
+    print("hegbherk")
 
 
 
